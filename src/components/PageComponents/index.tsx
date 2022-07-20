f50f81e63e719ec945e2960f498e37dbeb5b64c9
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2022-07-20 15:42:44 +0900

    style: title3

--- src/components/PageComponents/index.tsx
+++ src/components/PageComponents/index.tsx
@@ -3,4 +3,5 @@ import styled from 'styled-components';
 export const Text = styled.div``;
 export const Title1 = styled.div``;
 export const Title2 = styled.div``;
+export const Title3 = styled.div``;
 export const Divider = styled.div``;
