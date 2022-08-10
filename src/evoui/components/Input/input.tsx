0be8736633d2f71c27f56b585ce227e2055a340f
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2022-08-10 17:26:35 +0900

    feature: selectAll, isDisabled & refactoring

--- src/evoui/components/Input/input.tsx
+++ src/evoui/components/Input/input.tsx
@@ -1,21 +1,14 @@
-import { MouseEvent, useRef, useState } from 'react';
+import { useState, useRef, ChangeEvent, FocusEvent, MouseEvent } from 'react';
 import styled from 'styled-components';
 import {
-  IconPropsType,
-  InputPropsType,
   PropsType,
   RootPropsType,
+  InputPropsType,
+  ClearButtonPropsType,
+  ValueVisibleButtonPropsType,
 } from './input.type';
 
 const Root = styled.div<RootPropsType>`
-  display: flex;
-  align-items: center;
-  column-gap: 12px;
-  padding: 10px 12px;
-  background-color: ${(props) =>
-    !props.isFocused && props.isError
-      ? props.theme.evoui.colors.input.errorBgColor
-      : props.theme.evoui.colors.input.bgColor};
   border: 2px solid
     ${(props) =>
       props.isFocused
@@ -24,17 +17,27 @@ const Root = styled.div<RootPropsType>`
         ? props.theme.evoui.colors.input.errorBorderColor
         : props.theme.evoui.colors.input.borderColor};
   border-radius: 6px;
-  transition: border 0.2s, background-color 0.2s;
-  cursor: text;
+  padding: 10px 12px;
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  column-gap: 12px;
+  background-color: ${(props) =>
+    props.isError
+      ? props.theme.evoui.colors.input.errorBgColor
+      : props.theme.evoui.colors.input.bgColor};
+  opacity: ${(props) => (props.isDisabled ? '0.3' : '1')};
+  transition: all ease-in-out 300ms;
+  cursor: ${(props) => (props.isDisabled ? 'not-allowed' : 'text')};
 
   ${(props) => props.cssStyle ?? ''};
 `;
 const Input = styled.input<InputPropsType>`
+  border: none;
   all: unset;
-  flex-grow: 1;
+  flex: 1;
   min-width: 0;
-  height: 20px;
-  line-height: 20px;
   font-size: 16px;
 
   &::placeholder {
@@ -44,33 +47,58 @@ const Input = styled.input<InputPropsType>`
   ${(props) =>
     props.isTypeNumber
       ? `
-  /* Chrome, Safari, Edge, Opera */
-  &::-webkit-outer-spin-button,
-  &::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
+          /* Chrome, Safari, Edge, Opera */
+          &::-webkit-outer-spin-button,
+          &::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+          }
 
-  /* Firefox */
-  & {
-    -moz-appearance: textfield;
-  }
-  `
+          /* Firefox */
+          & {
+            -moz-appearance: textfield;
+          }
+        `
       : ''}
 
   ${(props) => props.cssStyle ?? ''};
 `;
-const Icon = styled.svg<IconPropsType>`
-  min-width: 16px;
+
+const Button = styled.button`
+  padding: 0;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 16px;
   height: 16px;
-  fill: ${(props) => props.theme.evoui.colors.input.iconFillColor};
+  min-width: 16px;
+  background-color: transparent;
   cursor: pointer;
 
-  ${(props) => props.cssStyle ?? ''};
+  > svg {
+    width: 100%;
+    fill: ${(props) => props.theme.evoui.colors.input.iconFillColor};
+    fill-rule: evenodd;
+    clip-rule: evenodd;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 14px;
+    height: 14px;
+    min-width: 14px;
+  }
+`;
+
+const ClearButton = styled(Button)<ClearButtonPropsType>`
+  ${(props) => props?.cssStyle ?? ''};
+`;
+
+const ValueVisibleButton = styled(Button)<ValueVisibleButtonPropsType>`
+  ${(props) => props?.cssStyle ?? ''};
 `;
 
 const createNoBlurringOnClick = <
-  elementType extends SVGSVGElement | HTMLDivElement,
+  elementType extends HTMLButtonElement | HTMLDivElement,
 >(
   onClick: (() => void) | ((event: MouseEvent<elementType>) => void),
 ) => ({
@@ -82,6 +110,7 @@ const createNoBlurringOnClick = <
   },
   onClick,
 });
+
 const createNoPropagatingOnClick = <elementType extends HTMLInputElement>(
   onClick: (() => void) | ((event: MouseEvent<elementType>) => void),
 ) => ({
@@ -96,15 +125,17 @@ const createNoPropagatingOnClick = <elementType extends HTMLInputElement>(
 
 export default function PureInput({
   type = 'text',
-  placeholder = '',
   value,
+  placeholder,
+  maxLength,
   onChange,
+  onKeyDown,
   onFocus,
   onBlur,
-  onKeyDown,
-  isClearable = false,
+  selectAll = false,
+  clearable = false,
   isError = false,
-  maxLength,
+  isDisabled = false,
   overrides,
 }: PropsType) {
   // Root 클릭 시 input에 focus를 해야하기 때문에 사용
@@ -114,131 +145,136 @@ export default function PureInput({
   const [isFocused, setIsFocused] = useState(false);
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
-  const focus = () => {
-    if (inputRef.current === null) {
-      return;
+  // type의 종류가 늘어날 수 있어 조건식을 직관적으로 작성했습니다.
+  const inputType = type === 'password' && isPasswordVisible ? 'text' : type;
+
+  const onRootClick = (): void => {
+    if (!!inputRef?.current) {
+      if (!!selectAll) {
+        inputRef.current.select();
+      } else {
+        inputRef.current.focus();
+      }
     }
-    inputRef.current.focus();
   };
-  const clear = () => {
-    const input = inputRef.current;
-    if (input === null) {
-      return;
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const isValueExist: boolean = typeof e?.currentTarget?.value === 'string';
+    const isValueLowerThanMaxLength: boolean =
+      typeof maxLength === 'number'
+        ? e.currentTarget.value.length <= maxLength
+        : true;
+
+    if (!isDisabled && isValueExist && isValueLowerThanMaxLength) {
+      onChange(e.currentTarget.value);
     }
-    // TODO: 사실 왜 동작하는지 모르는 코드
-    const valueSetter = Object.getOwnPropertyDescriptor(input, 'value')?.set;
-    const prototypeValueSetter = Object.getOwnPropertyDescriptor(
-      Object.getPrototypeOf(input),
-      'value',
-    )?.set;
-    if (valueSetter && valueSetter !== prototypeValueSetter) {
-      prototypeValueSetter?.call(input, '');
-    } else {
-      valueSetter?.call(input, '');
+  };
+
+  const onInputFocus = (e: FocusEvent<HTMLInputElement>): void => {
+    if (selectAll) {
+      e.currentTarget.select();
     }
-    input.dispatchEvent(new Event('input', { bubbles: true }));
+    if (onFocus) {
+      onFocus(e);
+    }
+    setIsFocused(true);
+  };
+
+  const onInputBlur = (e: FocusEvent<HTMLInputElement>): void => {
+    if (onBlur) {
+      onBlur(e);
+    }
+    setIsFocused(false);
+  };
+
+  const doClearInput = (): void => {
+    if (!isDisabled) {
+      onChange('');
+    }
+  };
+
+  const toggleIsPasswordVisible = (): void => {
+    setIsPasswordVisible((prev) => !prev);
   };
 
   return (
     <Root
+      isFocused={!isDisabled && isFocused}
+      isError={!isFocused && isError}
+      isDisabled={isDisabled}
+      {...createNoBlurringOnClick(onRootClick)}
       {...(typeof overrides?.Root?.css === 'string'
         ? {
             cssStyle: overrides.Root.css,
             ...(overrides.Root ?? {}),
           }
-        : overrides?.Root == undefined
+        : overrides?.Root === undefined
         ? {}
-        : {
-            style: overrides.Root.css,
-            ...overrides.Root,
-          })}
-      isFocused={isFocused}
-      isError={isError}
-      {...createNoBlurringOnClick(focus)}>
+        : { style: overrides.Root.css, ...overrides.Root })}>
       <Input
+        ref={inputRef}
+        tabIndex={isDisabled ? -1 : undefined}
+        type={inputType}
+        value={value}
+        placeholder={placeholder}
+        maxLength={maxLength}
+        readOnly={isDisabled}
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        onFocus={onInputFocus}
+        onBlur={onInputBlur}
+        isTypeNumber={type === 'number'}
+        {...createNoPropagatingOnClick((event) => event.stopPropagation())}
         {...(typeof overrides?.Input?.css === 'string'
           ? {
               cssStyle: overrides.Input.css,
               ...(overrides.Input ?? {}),
             }
-          : overrides?.Input == undefined
+          : overrides?.Input === undefined
           ? {}
-          : {
-              style: overrides.Input.css,
-              ...overrides.Input,
-            })}
-        ref={inputRef}
-        // type의 종류가 늘어날 수 있어 조건식을 직관적으로 작성했습니다.
-        type={type === 'password' && isPasswordVisible ? 'text' : type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        onFocus={(event) => {
-          onFocus?.(event);
-          setIsFocused(true);
-        }}
-        onBlur={(event) => {
-          onBlur?.(event);
-          setIsFocused(false);
-        }}
-        onKeyDown={onKeyDown}
-        {...createNoPropagatingOnClick((event) => event.stopPropagation())}
-        isTypeNumber={type === 'number'}
-        maxLength={maxLength}
+          : { style: overrides.Input.css, ...overrides.Input })}
       />
-      {isClearable && isFocused && value.length > 0 && (
-        <Icon
-          {...(typeof overrides?.Icon?.css === 'string'
+      {clearable && !isDisabled && isFocused && value.length > 0 && (
+        <ClearButton
+          {...createNoBlurringOnClick(doClearInput)}
+          {...(typeof overrides?.ClearButton?.css === 'string'
             ? {
-                cssStyle: overrides.Icon.css,
-                ...(overrides.Icon ?? {}),
+                cssStyle: overrides.ClearButton.css,
+                ...(overrides.ClearButton ?? {}),
               }
-            : overrides?.Icon == undefined
+            : overrides?.ClearButton == undefined
             ? {}
             : {
-                style: overrides.Icon.css,
-                ...overrides.Icon,
-              })}
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          {...createNoBlurringOnClick(clear)}>
-          <path d='M0 0h24v24H0z' fill='none' />
-          <path d='M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z' />
-        </Icon>
+                style: overrides.ClearButton.css,
+                ...overrides.ClearButton,
+              })}>
+          <svg viewBox='0 0 24 24'>
+            <path d='M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z' />
+          </svg>
+        </ClearButton>
       )}
       {type === 'password' && (
-        <Icon
-          {...(typeof overrides?.Icon?.css === 'string'
+        <ValueVisibleButton
+          {...createNoBlurringOnClick(toggleIsPasswordVisible)}
+          {...(typeof overrides?.ValueVisibleButton?.css === 'string'
             ? {
-                cssStyle: overrides.Icon.css,
-                ...(overrides.Icon ?? {}),
+                cssStyle: overrides.ValueVisibleButton.css,
+                ...(overrides.ValueVisibleButton ?? {}),
               }
-            : overrides?.Icon == undefined
+            : overrides?.ValueVisibleButton == undefined
             ? {}
             : {
-                style: overrides.Icon.css,
-                ...overrides.Icon,
-              })}
-          xmlns='http://www.w3.org/2000/svg'
-          viewBox='0 0 24 24'
-          {...createNoBlurringOnClick(() =>
-            setIsPasswordVisible((prev) => !prev),
-          )}>
-          {isPasswordVisible ? (
-            <>
-              <path d='M0 0h24v24H0z' fill='none' />
+                style: overrides.ValueVisibleButton.css,
+                ...overrides.ValueVisibleButton,
+              })}>
+          <svg viewBox='0 0 24 24'>
+            {isPasswordVisible ? (
               <path d='M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z' />
-            </>
-          ) : (
-            <>
-              <path
-                d='M0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0zm0 0h24v24H0z'
-                fill='none'
-              />
+            ) : (
               <path d='M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z' />
-            </>
-          )}
-        </Icon>
+            )}
+          </svg>
+        </ValueVisibleButton>
       )}
     </Root>
   );
--- src/evoui/components/Input/input.type.ts
+++ src/evoui/components/Input/input.type.ts
@@ -1,32 +1,37 @@
-import { ChangeEvent, FocusEvent, KeyboardEvent } from 'react';
+import { FocusEvent, KeyboardEvent } from 'react';
 import { DefaultOverridesType, DefaultPropsType } from '../global.type';
 
 export interface RootPropsType extends DefaultPropsType {
   isFocused: boolean;
   isError: boolean;
+  isDisabled: boolean;
 }
 export interface InputPropsType extends DefaultPropsType {
   isTypeNumber: boolean;
 }
-export type IconPropsType = DefaultPropsType;
+export type ClearButtonPropsType = DefaultPropsType;
+export type ValueVisibleButtonPropsType = DefaultPropsType;
 export interface PropsType {
   type?: 'text' | 'password' | 'email' | 'number' | 'tel';
-  placeholder?: string;
   value: string;
-  onChange: (() => void) | ((event: ChangeEvent<HTMLInputElement>) => void);
+  placeholder?: string;
+  maxLength?: number;
+  onChange: (() => void) | ((value: string) => void);
+  onKeyDown?: (() => void) | ((event: KeyboardEvent<HTMLInputElement>) => void);
   onFocus?:
     | (() => void)
     | ((event: FocusEvent<HTMLInputElement, Element>) => void);
   onBlur?:
     | (() => void)
     | ((event: FocusEvent<HTMLInputElement, Element>) => void);
-  onKeyDown?: (() => void) | ((event: KeyboardEvent<HTMLInputElement>) => void);
-  isClearable?: boolean;
+  selectAll?: boolean;
+  clearable?: boolean;
   isError?: boolean;
-  maxLength?: number;
+  isDisabled?: boolean;
   overrides?: {
     Root?: DefaultOverridesType;
     Input?: DefaultOverridesType;
-    Icon?: DefaultOverridesType;
+    ClearButton?: DefaultOverridesType;
+    ValueVisibleButton?: DefaultOverridesType;
   };
 }
