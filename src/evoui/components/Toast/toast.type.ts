ff5f464dccca767483343857b2e7a06132f27dbd
sehvdi <eric250@naver.com>
2022-08-11 10:25:55 +0900

    refactor(Toast/toast.type): IndependentToastPropsType & OptionalIndependentToastPropsType

--- src/evoui/components/Toast/toast.type.ts
+++ src/evoui/components/Toast/toast.type.ts
@@ -1,32 +1,35 @@
 import { DefaultOverridesType, DefaultPropsType } from '../global.type';
 
+type OptionalIndependentToastPropsType = Partial<{
+  /** Content of the toast.*/
+  content: string | React.ReactInstance | React.ReactElement;
+  /** Type of the toast. It must be one of 'success', 'error', 'info', 'warning'. Default value is 'info'. */
+  type: ToastType;
+  /** The duration how long the toast will be displayed. */
+  duration: number;
+  /** Is the toast could be closed. */
+  closable: boolean;
+  /** If set, the default icon will be replaced. */
+  icon: string | React.ReactInstance | React.ReactElement;
+  /** If set, the default close button icon be replaced. */
+  closeButton: string | React.ReactInstance | React.ReactElement;
+  /** UI elements props will be overridden. Only css value is available on current version. */
+  overrides: Partial<{
+    Root: DefaultOverridesType;
+    ToastContent: DefaultOverridesType;
+    Header: DefaultOverridesType;
+    Icon: DefaultOverridesType;
+    Title: DefaultOverridesType;
+    CloseButton: DefaultOverridesType;
+    Content: DefaultOverridesType;
+  }>;
+}>;
+
 // internal types: 패키지 외부에서 사용 불가
-export interface IndependentToastPropsType {
+export type IndependentToastPropsType = OptionalIndependentToastPropsType & {
   /** Title of the toast. It must be a string.*/
   title: string;
-  /** Content of the toast.*/
-  content?: string | React.ReactInstance | React.ReactElement;
-  /** Type of the toast. It must be one of 'success', 'error', 'info', 'warning'. Default value is 'info'. */
-  type?: ToastType;
-  /** The duration how long the toast will be displayed. */
-  duration?: number;
-  /** Is the toast could be closed. */
-  closable?: boolean;
-  /** If set, the default icon will be replaced. */
-  icon?: string | React.ReactInstance | React.ReactElement;
-  /** If set, the default close button icon be replaced. */
-  closeButton?: string | React.ReactInstance | React.ReactElement;
-  /** UI elements props will be overridden. Only css value is available on current version. */
-  overrides?: {
-    Root?: DefaultOverridesType;
-    ToastContent?: DefaultOverridesType;
-    Header?: DefaultOverridesType;
-    Icon?: DefaultOverridesType;
-    Title?: DefaultOverridesType;
-    CloseButton?: DefaultOverridesType;
-    Content?: DefaultOverridesType;
-  };
-}
+};
 export interface RootPropsType extends DefaultPropsType {}
 export interface ToastContentPropsType extends DefaultPropsType {
   type: ToastType;
