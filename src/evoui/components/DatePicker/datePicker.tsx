e6d104166ab6b26b40a366a89519a3597aae83dd
ardor1104 <87302777+ardor1104@users.noreply.github.com>
2022-12-08 11:30:37 +0900

    fix: remove incorrect import

--- src/evoui/components/DatePicker/datePicker.tsx
+++ src/evoui/components/DatePicker/datePicker.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ClickAwayListener from '../ClickAwayListener';
 import Select from '../Select';
